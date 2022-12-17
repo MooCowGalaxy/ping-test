f5f9a13a1fc8dec08214d12cfdf692767ed8df83
MooCowGalaxy <64293406+MooCowGalaxy@users.noreply.github.com>
2022-12-16 23:00:15 -0800

    add display message when sending http request to websocket port

--- websocket.js
+++ websocket.js
@@ -1,7 +1,14 @@
+const http = require('node:http');
 const ws = require('ws');
 const config = require('./config');
 
-const wss = new ws.WebSocketServer({ port: config.websocketPort });
+const listener = (req, res) => {
+    res.writeHead(200);
+    res.end("Hello world! This server is online and properly running!");
+};
+
+const httpServer = http.createServer(listener);
+const wss = new ws.WebSocketServer({ server: httpServer });
 
 wss.on('connection', (ws) => {
     ws.on('message', message => {
@@ -15,4 +22,6 @@ wss.on('connection', (ws) => {
     });
 });
 
-console.log(`Websocket server listening at ${config.websocketPort}`);
+httpServer.listen(config.websocketPort, () => {
+    console.log(`Websocket server listening at localhost:${config.websocketPort}`);
+});
